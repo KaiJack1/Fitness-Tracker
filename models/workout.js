088d92481b228bee6daf1af846552ba7c9235548
Kaid <kjjackson261@gmail.com>
2021-04-16 23:52:03 -0700

    update

---
+++ models/workout.js
@@ -0,0 +1,53 @@
+//Importing mongoose dependency
+const mongoose = require("mongoose");
+//creating workout schema.
+const Schema = mongoose.Schema;
+//Workout schema
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  //Creating functionality for the exercises.
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
+
+});
+//Workout model.
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = {
+    Workout: require("./Workout"),
+  };
+
+module.exports = Workout;
